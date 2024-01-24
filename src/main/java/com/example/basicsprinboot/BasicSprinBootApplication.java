32dfa9e8af716635f217578544407380e0e7f830
Vinul Vinsura <vinulviksura16@gmail.com>
2024-01-25 00:58:10 +0530

    Create SpringBoot Application

---
+++ src/main/java/com/example/basicsprinboot/BasicSprinBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.basicsprinboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSprinBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicSprinBootApplication.class, args);
+    }
+
+}
